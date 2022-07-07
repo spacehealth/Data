1199aab91a9db0bc48f63d2466165399f1b45654
Michael Roubeiz <michaelroubeiz@gmail.com>
2022-07-07 18:14:15 -0400

    Frontend template for app

--- flutter_base/android/app/src/main/kotlin/com/example/sidebar/MainActivity.kt
+++ flutter_base/android/app/src/main/kotlin/com/example/sidebar/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.flutter_base
+package com.example.sidebar
 
 import io.flutter.embedding.android.FlutterActivity
 
--- flutter_base/test/widget_test.dart
+++ flutter_base/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_base/main.dart';
+import 'package:sidebar/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
