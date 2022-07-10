fba6bf09b469468cbc9dc4cd8db0185fced5a639
Michael Roubeiz <michaelroubeiz@gmail.com>
2022-07-10 17:28:27 -0400

    Added button and button disappear

--- flutter_base/lib/main.dart
+++ flutter_base/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         child: MaterialApp(
           debugShowCheckedModeBanner: false,
           title: title,
-          theme: ThemeData(primarySwatch: Colors.deepOrange),
+          theme: ThemeData(primarySwatch: Colors.red),
           home: MainPage(),
         ),
       );
@@ -35,13 +35,32 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  bool _isVisible = true;
+
   @override
   Widget build(BuildContext context) => Scaffold(
-        // drawer: NavigationDrawerWidget(),
-        appBar: AppBar(
-          backgroundColor: Colors.red,
-          title: Text(MyApp.title),
-          centerTitle: true,
-        ),
-      );
+      // drawer: NavigationDrawerWidget(),
+      appBar: AppBar(
+        backgroundColor: Colors.grey.shade800,
+        title: Text(MyApp.title),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Visibility(
+              visible: _isVisible,
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(200.0)),
+                  minimumSize: Size(300, 300),
+                ),
+                label: Text("Start CPR", style: TextStyle(fontSize: 50)),
+                icon: Icon(Icons.emergency_outlined, size: 50),
+                onPressed: () {
+                  setState(() {
+                    _isVisible = false;
+                  });
+                  //Backend
+                },
+              ))));
 }
